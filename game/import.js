6b1dcbeae456a5423baf8a9e85a044c6bb342978
roberto freire <robertofreireferrazpassos@gmail.com>
2025-10-04 14:45:27 -0300

    add more colors

--- game/import.js
+++ game/import.js
@@ -14,8 +14,14 @@ export const images = {
 export const palette = {
   primary: 'rgba(106,190,238,1)',
   secondary: 'rgba(0,88,185,1)', 
-  dark : 'rgba(0,31,79,1)', 
+  dark : 'rgba(0,31,79,1)',
+  green : 'rgba(67,169,158,1)',
+  pink : 'rgba(166,100,213,1)',
+  brown : 'rgba(143,126,136,1)',
   backgroundprimary : 'rgba(106,190,238,0.6)',
   backgroundsecondary: 'rgba(0,88,185,0.6)',
   backgrounddark : 'rgba(0,31,79,0.6)',
+  green : 'rgba(67,169,158,0.6)',
+  pink : 'rgba(166,100,213,0.6)',
+  brown : 'rgba(143,126,136,0.6)',
 }
