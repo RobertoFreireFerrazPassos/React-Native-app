93cf99f20605617e33c595e5f5e445a9a34a139a
roberto freire <robertofreireferrazpassos@gmail.com>
2025-10-12 20:16:27 -0300

    fix gameloop

--- game/components/GameLoop.js
+++ game/components/GameLoop.js
@@ -4,20 +4,21 @@ import CustomText from "./CustomText";
 export default function GameLoop({ children }) {
   const [time, setTime] = useState(0);
   const lastFrameTime = useRef(performance.now());
-  const accumulator = useRef(0);
+  const frameInterval = 250;
+  const accumulatedTime = useRef(0);
 
   useEffect(() => {
     let animationFrameId;
-    const frameInterval = 1000 / 4;
 
     const update = (now) => {
       const delta = now - lastFrameTime.current;
       lastFrameTime.current = now;
-      accumulator.current += delta;
+      accumulatedTime.current += delta;
 
-      if (accumulator.current >= frameInterval) {
-        setTime((prev) => prev + accumulator.current);
-        accumulator.current = 0;
+
+      while (accumulatedTime.current >= frameInterval) {
+        setTime((prev) => +(prev + frameInterval).toFixed(2));
+        accumulatedTime.current -= frameInterval;
       }
 
       animationFrameId = requestAnimationFrame(update);
@@ -30,7 +31,9 @@ export default function GameLoop({ children }) {
   return (
     <>
       {children}
-      <CustomText>Elapsed Time: {(time / 1000).toFixed(2)}s</CustomText>
+      <CustomText>
+        Elapsed Time: {(time / 1000).toFixed(2)}s
+      </CustomText>
     </>
   );
 }
